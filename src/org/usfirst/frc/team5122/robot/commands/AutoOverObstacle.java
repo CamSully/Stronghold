a3a75646366016b80ed8360393cde9fa77c80f6a
Blake Bourque <Techplex.Engineer@gmail.com>
2016-02-05 15:22:35 -0500

    Initial code

---
+++ src/org/usfirst/frc/team5122/robot/commands/AutoOverObstacle.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team5122.robot.commands;
+
+import org.usfirst.frc.team5122.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutoOverObstacle extends Command {
+
+    public AutoOverObstacle() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drivetrain);
+    	
+    	setTimeout(1.9);
+    }
+
+    double speed = -0.85;
+	double turn = 0.27;
+	
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.drivetrain.drive(speed, turn);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.drivetrain.stopDrivetrain();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
